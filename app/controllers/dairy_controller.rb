6554186ade9397ea871cc3b093a655c74d885b3b
大塚雄介 <yusuke@ootsukayuusukenoMacBook-Pro.local>
2018-02-10 17:21:32 +0900

    バリデーションのエラーメッセージ

--- app/controllers/dairy_controller.rb
+++ app/controllers/dairy_controller.rb
@@ -11,15 +11,14 @@ def create
 
 
   #create!メソッドは検証が通らなかった際にエラーを返す
-  newdairy = Dairy.create!(name: current_user.nickname, text: dairy_params[:text], label: "D",
-    user_id: current_user.id, title: dairy_params[:title], tag: dairy_params[:field])
+  newdairy = Dairy.create!(dairy_params)
 
   #以下、投稿にタグを関連付けていく。入力フォームに空白があっても対応させている。
-  tags = dairy_params[:field]
+  tags = newdairy.tag
 
   f = 0
   while f <= 4 do
-   if tags[f] != "" 
+   if tags[f] != ""
     existTag = Tag.find_by(tag: tags[f])
      if existTag == nil
       tag = Tag.create!(tag: tags[f])
@@ -43,6 +42,9 @@ end
 def posting
     redirect_to action: :index unless user_signed_in?
   #サインインしてない状態で投稿しようとした場合、indexに返す
+
+  @post = Dairy.new
+
 end
 
 def show
@@ -62,10 +64,10 @@ def like_delete
   @data = Dairy.find(params[:id])
 end
 
-#配列がparamsに入っている時の記述に注意
+#paramsで配列が送られてきたとき、カラム名:[]のようにすると、カラムに配列を格納できる
 private
 def dairy_params
-  params.permit(:text, :title, field:[])
+  params.require(:dairy).permit(:text, :title, tag:[]).merge(name: current_user.nickname, label: "D", user_id: current_user.id)
 end
 
 
--- app/models/dairy.rb
+++ app/models/dairy.rb
@@ -11,7 +11,6 @@ class Dairy < ActiveRecord::Base
   validates :text, presence: true
   validates :title, presence: true, length: {mininum: 1, maximum: 100 }
   validate :tag_content?
-  validate :tag_length?
    #ユーザーがいいね！しているかどうか確認するメソッド
 
   def like_user(user_id)
@@ -19,18 +18,8 @@ class Dairy < ActiveRecord::Base
     likes.find_by(user_id: user_id)
   end
 
-#タグ配列のいずれかが20文字を超えていないか？の検証
-private
-  def tag_length?
-    f=0
-    while f <= 4 do
-     if tag[f].length > 20          #命名規則のためか、ここでtagsとするとnilclassエラーになる
-      errors.add(:tag, "タグ#{f+1}が20文字を超えています！")
-     end
-    f += 1
-    end
-  end
 
+private
 #タグ配列の全て空でないか？の検証
   def tag_content?
     if tag[0]=="" && tag[1]=="" && tag[2]=="" && tag[3]=="" && tag[4]==""
